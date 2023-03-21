dcb2a3ff04777c6cd1a97b1850c27b72bde23952
raiha <68338762+raihalea@users.noreply.github.com>
2023-03-21 16:56:36 +0900

    add EKS

---
+++ aws_guardduty_tester/eks_fargate_stack.py
@@ -0,0 +1,44 @@
+from aws_cdk import (
+    # Duration,
+    Stack,
+    CfnOutput,
+    aws_ec2 as ec2,
+    aws_ecs as ecs,
+    aws_eks as eks,
+    aws_ecr_assets as assets,
+    aws_iam as iam,
+)
+from constructs import Construct
+
+
+class EksFargateStack(Stack):
+    def __init__(self, scope: Construct, construct_id: str, base, **kwargs) -> None:
+        super().__init__(scope, construct_id, **kwargs)
+
+        cluster = eks.FargateCluster(
+            self,
+            "Cluster",
+            vpc=base.vpc,
+            version=eks.KubernetesVersion.V1_24,
+        )
+
+        cluster.admin_role.add_to_policy(
+            iam.PolicyStatement(
+                effect=iam.Effect.ALLOW, resources=["*"], actions=["eks:*"]
+            )
+        )
+
+        ecr_asset = assets.DockerImageAsset(self, "tester", directory="./tester")
+        image_url = ecr_asset.image_uri
+
+        cluster.add_manifest(
+            "mypod",
+            {
+                "apiVersion": "v1",
+                "kind": "Pod",
+                "metadata": {"name": "mypod"},
+                "spec": {"containers": [{"name": "tester", "image": image_url}]},
+            },
+        )
+
+        CfnOutput(self, "EKSclusterRole", value=cluster.admin_role.role_name)
