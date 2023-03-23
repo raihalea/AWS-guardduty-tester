2ef898867db8f03395c4b6def3e88535ba1de1c3
raiha <68338762+raihalea@users.noreply.github.com>
2023-03-24 03:29:15 +0900

    add BatchFargate

--- aws_guardduty_tester/batch_fargate.py
+++ aws_guardduty_tester/batch_fargate.py
@@ -2,11 +2,12 @@ from aws_cdk import (
     Stack,
     aws_batch_alpha as batch,
     aws_ecs as ecs,
+    aws_iam as iam,
 )
 from constructs import Construct
 
 
-class BatchEc2Stack(Stack):
+class BatchFargateStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, base, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
@@ -18,9 +19,22 @@ class BatchEc2Stack(Stack):
                     compute_environment=batch.ComputeEnvironment(
                         self,
                         "Batch",
-                        compute_resources=batch.ComputeResources(vpc=base.vpc),
+                        compute_resources=batch.ComputeResources(
+                            vpc=base.vpc, type=batch.ComputeResourceType.FARGATE
+                        ),
                     ),
                     order=1,
+                )
+            ],
+        )
+
+        task_execution_role = iam.Role(
+            self,
+            "TaskExecutionRole",
+            assumed_by=iam.ServicePrincipal("ecs-tasks.amazonaws.com"),
+            managed_policies=[
+                iam.ManagedPolicy.from_aws_managed_policy_name(
+                    "service-role/AmazonECSTaskExecutionRolePolicy"
                 )
             ],
         )
@@ -28,12 +42,13 @@ class BatchEc2Stack(Stack):
         batch_jofDef = batch.JobDefinition(
             self,
             "jobDef",
+            platform_capabilities=[batch.PlatformCapabilities("FARGATE")],
             container=batch.JobDefinitionContainer(
                 image=ecs.ContainerImage.from_asset("./tester/"),
                 command=["dig", "guarddutyc2activityb.com"],
-                memory_limit_mib=6,
                 log_configuration=batch.LogConfiguration(
                     log_driver=batch.LogDriver.AWSLOGS
                 ),
+                execution_role=task_execution_role,
             ),
         )
